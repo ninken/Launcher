50e3a0508fd58a94a7d5529d6016dd3eafd2412f
ninken <ninken@gmail.com>
2023-05-18 23:04:57 -0500

    Add project files.

---
+++ Launcher/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnWebsite = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnTimer = new System.Windows.Forms.Button();
+            this.lbTimer = new System.Windows.Forms.Label();
+            this.btnApp = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(62, 13);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(204, 20);
+            this.txtUsername.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Username:";
+            // 
+            // btnWebsite
+            // 
+            this.btnWebsite.Location = new System.Drawing.Point(12, 108);
+            this.btnWebsite.Name = "btnWebsite";
+            this.btnWebsite.Size = new System.Drawing.Size(268, 36);
+            this.btnWebsite.TabIndex = 4;
+            this.btnWebsite.Text = "Launch Secret Server Website";
+            this.btnWebsite.UseVisualStyleBackColor = true;
+            this.btnWebsite.Click += new System.EventHandler(this.btnWebsite_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Password:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(62, 39);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(206, 20);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // btnTimer
+            // 
+            this.btnTimer.Location = new System.Drawing.Point(12, 149);
+            this.btnTimer.Name = "btnTimer";
+            this.btnTimer.Size = new System.Drawing.Size(268, 36);
+            this.btnTimer.TabIndex = 7;
+            this.btnTimer.Text = "Start Secret Server Timer";
+            this.btnTimer.UseVisualStyleBackColor = true;
+            this.btnTimer.Click += new System.EventHandler(this.btnTimer_Click);
+            // 
+            // lbTimer
+            // 
+            this.lbTimer.AutoSize = true;
+            this.lbTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTimer.Location = new System.Drawing.Point(82, 188);
+            this.lbTimer.Name = "lbTimer";
+            this.lbTimer.Size = new System.Drawing.Size(133, 36);
+            this.lbTimer.TabIndex = 8;
+            this.lbTimer.Text = "00:00:00";
+            // 
+            // btnApp
+            // 
+            this.btnApp.Location = new System.Drawing.Point(12, 66);
+            this.btnApp.Name = "btnApp";
+            this.btnApp.Size = new System.Drawing.Size(268, 36);
+            this.btnApp.TabIndex = 9;
+            this.btnApp.Text = "Launch Application ";
+            this.btnApp.UseVisualStyleBackColor = true;
+            this.btnApp.Click += new System.EventHandler(this.btnApp_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(271, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(21, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "⊙";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonTogglePassword_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 227);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnApp);
+            this.Controls.Add(this.lbTimer);
+            this.Controls.Add(this.btnTimer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.btnWebsite);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUsername);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "RunAS Launcher";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnWebsite;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnTimer;
+        private System.Windows.Forms.Label lbTimer;
+        private System.Windows.Forms.Button btnApp;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Launcher/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Security;
+using System.Runtime.InteropServices;
+using System.Drawing;
+
+namespace Launcher
+{
+    public partial class Form1 : Form
+    {
+        private bool passwordVisible = false;
+        public DateTime starttime;
+        public int serverhours;
+        public string apppath = "";
+        public string sswebsite = "";
+        public string radiusapp = "";
+        private Timer countdownTimer;
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        public Form1()
+        {
+            InitializeComponent();
+            txtPassword.UseSystemPasswordChar = true;
+            countdownTimer = new Timer();
+            countdownTimer.Interval = 250;
+            countdownTimer.Tick += CountdownTimer_Tick;
+        }
+
+        private void CountdownTimer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan remainingTime = starttime.AddHours(serverhours) - DateTime.Now;
+            if (remainingTime.TotalSeconds <= 0)
+            {
+                countdownTimer.Stop();
+                lbTimer.Text = "00:00";
+            }
+            else
+            {
+                lbTimer.Text = remainingTime.ToString(@"hh\:mm\:ss");
+            }
+        }
+
+        private void TogglePasswordVisibility()
+        {
+            passwordVisible = !passwordVisible;
+            txtPassword.UseSystemPasswordChar = !passwordVisible;
+        }
+
+        private void buttonTogglePassword_Click(object sender, EventArgs e)
+        {
+            TogglePasswordVisibility();
+        }
+
+        private void btnWebsite_Click(object sender, EventArgs e)
+        {
+            Process.Start(sswebsite);
+        }
+
+        private void btnApp_Click(object sender, EventArgs e)
+        {
+            if (radiusapp != "")
+            {
+                radiusapp = System.IO.Path.GetFileNameWithoutExtension(radiusapp);
+                Process[] processes = Process.GetProcessesByName(radiusapp);
+                if (processes.Length > 0)
+                {
+                    try
+                    {
+                        IntPtr hWnd = processes[0].MainWindowHandle;
+                        SetForegroundWindow(hWnd);
+                    }
+                    catch
+                    {//
+                    }
+                }
+                else
+                {
+                    Process.Start(radiusapp);
+                }
+            }
+
+            //Run AS
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = apppath,
+                UserName = txtUsername.Text,
+                Domain = Environment.UserDomainName,
+                UseShellExecute = false
+            };
+                string mypass = txtPassword.Text;
+
+        SecureString securePassword = new SecureString();
+            foreach (char c in mypass)
+            {
+                securePassword.AppendChar(c);
+            }
+        startInfo.Password = securePassword;
+            Process.Start(startInfo);
+        }
+
+        private void btnTimer_Click(object sender, EventArgs e)
+        {
+            starttime = DateTime.Now;
+            countdownTimer.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Center the form on the screen
+            this.StartPosition = FormStartPosition.Manual;
+            int screenWidth = Screen.PrimaryScreen.Bounds.Width;
+            int screenHeight = Screen.PrimaryScreen.Bounds.Height;
+            int formWidth = this.Width;
+            int formHeight = this.Height;
+            int left = (screenWidth - formWidth) / 2;
+            int top = (screenHeight - formHeight) / 2;
+            this.Location = new Point(left, top);
+
+            txtUsername.Text = ConfigurationManager.AppSettings["Username"];
+            txtPassword.Text = ConfigurationManager.AppSettings["Password"];
+            DateTime.TryParse(ConfigurationManager.AppSettings["StartTime"], out starttime);
+            int.TryParse(ConfigurationManager.AppSettings["ServerHours"], out serverhours);
+            apppath = ConfigurationManager.AppSettings["apppath"];
+            sswebsite = ConfigurationManager.AppSettings["sswebsite"];
+            radiusapp = ConfigurationManager.AppSettings["radiusapp"];
+            
+            if (sswebsite == "")
+            {
+                btnWebsite.Visible = false;
+                btnTimer.Visible = false;
+                lbTimer.Visible = false;
+                this.Height = this.Height - 100;
+            }
+
+            if (apppath == "")
+            {
+                btnApp.Visible = false;
+            }
+
+            if (DateTime.Now < starttime.AddHours(serverhours))
+            {
+                countdownTimer.Start();
+                TimeSpan remainingTime = starttime.AddHours(serverhours) - DateTime.Now;
+                lbTimer.Text = remainingTime.ToString(@"hh\:mm\:ss");
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Save values to app.config
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings["Username"].Value = txtUsername.Text;
+            config.AppSettings.Settings["Password"].Value = txtPassword.Text;
+            config.AppSettings.Settings["StartTime"].Value = starttime.ToString("yyyy-MM-dd HH:mm:ss");
+            config.AppSettings.Settings["apppath"].Value = apppath;
+            config.AppSettings.Settings["sswebsite"].Value = sswebsite;
+            config.AppSettings.Settings["radiusapp"].Value = radiusapp;
+            config.AppSettings.Settings["serverhours"].Value = serverhours.ToString();
+            config.Save(ConfigurationSaveMode.Modified);
+        }
+    }
+}
